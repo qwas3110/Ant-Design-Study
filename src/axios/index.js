ea0487b24eddd56ca2884f492492b168d062a8af
xiehongwei <283994917@qq.com>
2019-10-15 14:18:47 +0800

    项目工程化之动态表格渲染 2 -3

--- src/axios/index.js
+++ src/axios/index.js
@@ -43,6 +43,15 @@ export default class Axios {
   }
 
   static ajax(options) {
+    let loading;
+    if (options.data && options.data.isShowLoading !== false){
+      loading = document.getElementById('ajaxLoading');
+      loading.style.display = 'block';
+    }
+
+
+
+
     let baseUrl = 'https://www.easy-mock.com/mock/5a7278e28d0c633b9c4adbd7/api';
 
 
@@ -54,6 +63,11 @@ export default class Axios {
         timeout:5000,
         params: (options.data && options.data.params) || ''
       }).then((response) => {
+        if (options.data && options.data.isShowLoading !== false) {
+          loading = document.getElementById('ajaxLoading');
+          loading.style.display = 'none';
+        }
+
         if (response.status == '200') {
           let res = response.data;
           if (res.code == '0') {
--- src/pages/table/basicTable.js
+++ src/pages/table/basicTable.js
@@ -136,17 +136,43 @@ class BasicTable extends React.Component {
       {
         title: '性别',
         key: 'sex',
-        dataIndex: 'sex'
+        dataIndex: 'sex',
+        render(sex){
+          return sex ==1 ?'男':'女'
+        }
       },
       {
         title: '状态',
         key: 'state',
-        dataIndex: 'state'
+        dataIndex: 'state',
+        render(state){
+          let config  = {
+            '1':'咸鱼一条',
+            '2':'风华浪子',
+            '3':'北大才子',
+            '4':'百度FE',
+            '5':'创业者'
+          }
+          return config[state];
+        }
       },
       {
         title: '爱好',
         key: 'interest',
-        dataIndex: 'interest'
+        dataIndex: 'interest',
+        render(abc) {
+          let config = {
+            '1': '游泳',
+            '2': '打篮球',
+            '3': '踢足球',
+            '4': '跑步',
+            '5': '爬山',
+            '6': '骑行',
+            '7': '桌球',
+            '8': '麦霸'
+          }
+          return config[abc];
+        }
       },
       {
         title: '生日',
