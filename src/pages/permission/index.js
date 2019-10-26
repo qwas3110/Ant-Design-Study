771c2245667d2d8d49f8125207ba88670dd7b4a1
xhw <283994917@qq.com>
2019-10-26 15:36:45 +0800

    角色创建功能实现

--- src/pages/permission/index.js
+++ src/pages/permission/index.js
@@ -21,6 +21,31 @@ export default class PermissionUser extends React.Component {
     axios.requestList(this,'/role/list',{});
   }
 
+  // 打开创建角色弹框
+  handleRole=()=>{
+    this.setState({
+      isRoleVisible:true
+    })
+  };
+  // 角色提交
+  handleRoleSubmit=()=>{
+    let data = this.roleForm.props.form.getFieldsValue(); // 获取表单值
+    axios.ajax({
+      url:'role/create',
+      data:{
+        params:data
+      }
+    }).then((res)=>{
+      if(res.code ==0){
+        this.setState({
+          isRoleVisible:false // 关闭弹框
+        })
+        this.roleForm.props.form.resetFields(); // 重置表单
+        axios.requestList(this, '/role/list', {}); // 重新接口 渲染最新值
+      }
+    })
+  };
+
 
   render() {
     const columns = [
@@ -52,7 +77,7 @@ export default class PermissionUser extends React.Component {
     return (
       <div>
         <Card>
-          <Button type={"primary"}>创建角色</Button>
+          <Button type={"primary"} onClick={this.handleRole}>创建角色</Button>
           <Button type={"primary"} style={{marginLeft:10,marginRight:10}} >设置权限</Button>
           <Button type={"primary"}>用户授权</Button>
 
@@ -67,9 +92,57 @@ export default class PermissionUser extends React.Component {
           />
         </div>
 
+        <Modal
+          title="创建角色"
+          visible={this.state.isRoleVisible}
+          onOk={this.handleRoleSubmit}
+          onCancel={()=>{
+            this.roleForm.props.form.resetFields(); // 重置form表单
+            this.setState({
+              isRoleVisible:false
+            })
+          }}
+        >
+          <RoleForm wrappedComponentRef={(inst)=>this.roleForm=inst}></RoleForm>
+        </Modal>
+
 
 
       </div>
     );
   }
 }
+
+
+class RoleForm extends React.Component {
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    const formItemLayout = {
+      labelCol: { span: 5 },
+      wrapperCol: { span: 19 }
+    }
+    return (
+      <Form layout="horizontal">
+        <FormItem label="角色名称" {...formItemLayout}>
+          {
+            getFieldDecorator('role_name')(
+              <Input type="text" placeholder="请输入角色名称" />
+            )
+          }
+        </FormItem>
+        <FormItem label="状态" {...formItemLayout}>
+          {
+            getFieldDecorator('state')(
+              <Select>
+                <Option value={1}>开启</Option>
+                <Option value={0}>关闭</Option>
+              </Select>
+            )
+          }
+        </FormItem>
+      </Form>
+    );
+  }
+}
+RoleForm = Form.create({})(RoleForm);
